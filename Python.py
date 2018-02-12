e2e5f022860658959bbb247b5bd29d10a0f1730b
OnlyHyun <poetisa0930@naver.com>
2018-02-12 23:06:41 +0900

    Add *

--- Python.py
+++ Python.py
@@ -68,3 +68,10 @@ for i in range(1, 6):
     for j in range(0, i):
         print("*", end = " ")
     print(" ")
+
+for i in range(0, 5):
+    for j in range(0, i):
+        print(" ", end = " ")
+    for j in range(0, 5-i):
+        print("*", end = " ")
+    print(" ")   
