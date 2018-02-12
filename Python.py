bcbbb495703f63ade35702b722480567c63fefa6
OnlyHyun <poetisa0930@naver.com>
2018-02-12 23:13:14 +0900

    Add ***

--- Python.py
+++ Python.py
@@ -75,3 +75,29 @@ for i in range(0, 5):
     for j in range(0, 5-i):
         print("*", end = " ")
     print(" ")   
+
+for i in range(1, 6):
+    for j in range(0, 5-i):
+        print(" ", end = " ")
+    for j in range(0, i*2-1):
+        print("*", end = " ")
+    for j in range(0, 5-i):
+        print(" ", end = " ")
+    print(" ")
+
+for i in range(0, 5):
+    for j in range(0, i):
+        print(" ", end = " ")
+    for j in range(0, 9-2*i):
+        print("*", end = " ")
+    for j in range(0, i):
+        print(" ", end = " ")
+    print(" ")
+
+
+
+4, 1, 4
+3, 3, 3
+2, 5, 2
+1, 7, 1
+0, 9, 0
