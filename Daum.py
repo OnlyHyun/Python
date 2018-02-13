bf023ecc81e4ab4efb5a8d3e178635bf80fc3550
OnlyHyun <poetisa0930@naver.com>
2018-02-14 01:08:47 +0900

    Google Add

--- Daum.py
+++ Daum.py
@@ -1,7 +1,7 @@
+"""
 s = [1, 3, 4, 8, 13, 17, 20]
 
 s = sorted(s)
-
 min = max(s)
 
 i=0
@@ -15,7 +15,7 @@ while True:
     if(len(s) == i+1): break
 
 print(min_length)
-
+"""
     
 """
 for i in range(len(s)):
@@ -30,12 +30,8 @@ print(min_length)
 """
 
 
-"""
-
 s= [1, 3, 4, 8, 13, 17, 20]
 pairs = list(zip(s[0:], s[1:]))
 print(pairs)
 pairs.sort(key = lambda x:x[1]-x[0])
 print(pairs[0])
-
-"""
