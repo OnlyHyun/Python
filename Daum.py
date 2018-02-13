09b58a3546b350ecf0616e58512b5ff7cf244205
OnlyHyun <poetisa0930@naver.com>
2018-02-13 23:38:25 +0900

    Daum Exam

---
+++ Daum.py
@@ -0,0 +1,24 @@
+"""
+S = list(input("문자열 S의 값은?"))
+print(type(S))
+print(min(S))
+min_length = max(S) - min(S)
+min = max(S)
+
+for i in range(len(S)):
+    for j in range(i+1, len(S)):
+        length = abs(S[i] - S[j])
+        if(min > length):
+            min_length = [S[i], S[j]]
+            min = length
+
+print(min_length)            
+"""
+
+
+s= [1, 3, 4, 8, 13, 17, 20]
+pairs = list(zip(s[0:], s[1:]))
+print(pairs)
+pairs.sort(key = lambda x:x[1]-x[0])
+print(pairs[0])
+
