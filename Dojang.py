a1beb9d8d4366eb7f4beedc5d11400b927ea5c7a
OnlyHyun <poetisa0930@naver.com>
2018-02-14 01:47:27 +0900

    Dojangwow

--- Dojang.py
+++ Dojang.py
@@ -16,3 +16,27 @@ for i in range(1, 1001):
 
 for i in range(10):
     print("%s:%s개" % (i, a[i]))
+
+
+
+for x in range(10):
+    print('%d: %d'%(x,''.join(map(str, range(1, 1001))).count(str(x))))
+
+from collections import defaultdict
+
+d = defaultdict(int)
+for n in range(1, 1001):
+    for x in str(n):
+        d[x] += 1
+
+print(d)
+
+
+count={ x:0 for x in range(0,10) }
+
+for x in range(1,101):
+    for i in str(x):
+        count[int(i)]+=1
+        print(i)
+        
+print(count)
