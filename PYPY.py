d4642a0f4b328213a838450346dba70af8c93aa1
OnlyHyun <poetisa0930@naver.com>
2018-02-13 20:47:44 +0900

    Memo

--- PYPY.py
+++ PYPY.py
@@ -28,3 +28,18 @@ print((getTotalPage(15, 10)))
 print((getTotalPage(25, 10)))
 print((getTotalPage(30, 10)))
 
+import sys
+
+option = sys.argv[1]
+
+if option == '-a':
+    memo = sys.argv[2]
+    f = open('memo.txt', 'a')
+    f.write(memo)
+    f.write('\n')
+    f.close()
+elif option == '-v':
+    f = open('memo.txt', 'r')
+    print(f.readlines())
+    f.close()
+    
