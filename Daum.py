858c992f2c28bf617f805475c7ddea343abab888
OnlyHyun <poetisa0930@naver.com>
2018-02-13 23:41:04 +0900

    Daum My way

--- Daum.py
+++ Daum.py
@@ -1,20 +1,21 @@
-"""
-S = list(input("문자열 S의 값은?"))
-print(type(S))
-print(min(S))
-min_length = max(S) - min(S)
-min = max(S)
+s = [1, 3, 4, 8, 13, 17, 20]
 
-for i in range(len(S)):
-    for j in range(i+1, len(S)):
-        length = abs(S[i] - S[j])
+print(min(s))
+
+min_length = max(s) - min(s)
+min = max(s)
+
+for i in range(len(s)):
+    for j in range(i+1, len(s)):
+        length = abs(s[i] - s[j])
         if(min > length):
-            min_length = [S[i], S[j]]
+            min_length = [s[i], s[j]]
             min = length
 
 print(min_length)            
-"""
 
+
+"""
 
 s= [1, 3, 4, 8, 13, 17, 20]
 pairs = list(zip(s[0:], s[1:]))
@@ -22,3 +23,4 @@ print(pairs)
 pairs.sort(key = lambda x:x[1]-x[0])
 print(pairs[0])
 
+"""
