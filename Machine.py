08dca60655c525262ab48fcdaec87a504988696d
OnlyHyun <poetisa0930@naver.com>
2018-02-19 19:08:30 +0900

    Machinery

--- Machine.py
+++ Machine.py
@@ -1,7 +1,22 @@
 """
-방식이 for문 돌 때 계속해서
-sorted로 정리한 후에
-기존의 가성비와 추가됐을 때의 가성비를 비교
+
+가성비 최대화
+
 
 """
 
+s = int(input("원래 기계의 가격은?: "))
+s1 = int(input("원래 기계의 성능은?: "))
+s2 = int(input("추가 부품의 가격은?: "))
+s3 = str(input("추가 부품의 성능은?: ")).split()
+
+s3 = sorted(s3)
+s3.reverse()
+
+for i in s3:
+    if((s1/s) < ((s1+int(i))/(s+s2))):
+        s+=s2
+        s1+=int(i)
+    else : break
+
+print(s1//s)
