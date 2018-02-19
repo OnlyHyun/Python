79b0dcbfd70d9af9c16eb4111a1d8f42aee0ddc4
OnlyHyun <poetisa0930@naver.com>
2018-02-19 19:18:25 +0900

    Machine~

--- Machine.py
+++ Machine.py
@@ -10,8 +10,18 @@ s1 = int(input("원래 기계의 성능은?: "))
 s2 = int(input("추가 부품의 가격은?: "))
 s3 = str(input("추가 부품의 성능은?: ")).split()
 
+
 s3 = sorted(s3)
 s3.reverse()
+"""
+
+쉽게 하기
+
+s3.sort(reverse=True)
+내림차순으로 정렬
+
+
+"""
 
 for i in s3:
     if((s1/s) < ((s1+int(i))/(s+s2))):
@@ -20,3 +30,5 @@ for i in s3:
     else : break
 
 print(s1//s)
+
+
