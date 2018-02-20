5d6d42fdb73c86e6de18aba0cc83ace1e3db7c11
OnlyHyun <poetisa0930@naver.com>
2018-02-20 21:02:51 +0900

    재귀

---
+++ N진수.py
@@ -0,0 +1,39 @@
+"""
+
+10진수를 n진수로 변환하기
+
+n으로 나눈 나머지와 몫?을 계속 가지고 가야함
+
+
+"""
+
+def switch(num):
+    return{
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+        }.get(num, 10)
+
+Share = int(input("변환할 10진수는?: "))
+n = int(input("변환하고 싶은 n진수는?: "))
+
+result = []
+
+while True:
+
+    Remainder = Share%n
+    Share = Share//n
+
+    if(Remainder >= 10): Remainder = switch(Remainder)
+        
+    result.append(Remainder)
+
+    if(Share == 0):
+        result.reverse()
+        break
+
+    
+print(result)
