a7c71dd38d9d2b6064d8d8ad7e5f304cc81d318a
OnlyHyun <poetisa0930@naver.com>
2018-02-22 17:37:45 +0900

    Amazon

---
+++ Amazon.py
@@ -0,0 +1,23 @@
+S = ['a1','a2','a3','a4','a5','b1','b2','b3','b4','b5']
+S1 = [ 0 for i in range(0, len(S)) ]
+S2 = []
+count = 0
+
+while True:
+
+    S1[count] = 0
+    
+    if(S[0][0] == S[1][0]):
+        S1[count] = S.pop(0)
+        count+=1
+    else:
+        S1[count] = S.pop(0)
+        break
+    
+del S1[count+1:]
+
+for i in range(0, count+1):
+    S2.append(S1[i])
+    S2.append(S[i])
+
+print(S2)
